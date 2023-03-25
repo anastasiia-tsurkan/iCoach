d505aa029e46e0fdc203817e6072bb025b48aa03
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-25 17:17:02 +0200

    Staticfiles managing

--- sport_club/settings.py
+++ sport_club/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 import dj_database_url
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
