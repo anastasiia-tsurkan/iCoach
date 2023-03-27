7232f13b63de38873839daa5ff24d4e852a96626
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-27 13:41:10 +0300

    Change DEBUG settings

--- sport_club/settings.py
+++ sport_club/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+    # 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "https://icoach.onrender.com"]
 
