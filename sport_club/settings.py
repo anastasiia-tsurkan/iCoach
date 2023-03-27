177457d771e6d19a226ce190ea3bf20315cc5e7d
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-03-27 14:56:49 +0300

    Fix indentation before ALLOWED_HOSTS in settings

--- sport_club/settings.py
+++ sport_club/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-    # 'RENDER' not in os.environ
+# 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["icoach.onrender.com", "127.0.0.1"]
 
